7ff9be90a38ae276c0f09b7fa2502a90e1545187
Sumanta <Sumanta@Sumanta-PC.local>
2017-06-16 18:52:05 +0530

    New code

---
+++ src/test/java/com/test/TestClass.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class TestClass {
+	
+	public void mainTest(){
+		System.out.println("Hello World !!!");
+		System.out.println("Hello There !!!");
+		System.out.println("Hello There1 !!!");
+		
+	}
+}
